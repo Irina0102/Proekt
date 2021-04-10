03048887e86e455d3d934d9307da75b80944b212
Irina0102 <81677176+Irina0102@users.noreply.github.com>
2021-04-10 13:32:14 +0500

    Add files via upload

--- MyFerstApplication/app/src/main/java/com/example/myferstapplication/settings.java
+++ MyFerstApplication/app/src/main/java/com/example/myferstapplication/settings.java
@@ -11,40 +11,49 @@ import android.widget.Button;
 import static com.example.myferstapplication.ThemeManager.THEME_1;
 import static com.example.myferstapplication.ThemeManager.THEME_2;
 
-public class settings extends AppCompatActivity {
-    Settings context;
+public class settings extends AppCompatActivity
+{
+    settings context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ThemeManager.setTheme(this);
         setContentView(R.layout.activity_main3);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        context=this;
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        context = this;
+
+        {
+            Button item = findViewById(R.id.button);
+            View.OnClickListener buttonClickListener = new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    ThemeManager.changeTo(context, ThemeManager.THEME_1);
+                }
+            };
+            item.setOnClickListener(buttonClickListener);
         }
-        /**
-         * переход назад
-         */
-        @Override
-        public boolean onSupportNavigateUp() {
-            onBackPressed();
-            return true;
+        {
+            Button item = findViewById(R.id.button2);
+            View.OnClickListener buttonClickListener = new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    ThemeManager.changeTo(context, ThemeManager.THEME_2);
+
+                }
+            };
+            item.setOnClickListener(buttonClickListener);
+        }
+
+
+
     }
-    Button item=findViewById(R.id.button);
-        View.OnClickListener buttonClickListener=new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ThemeManager.changeTo(context, THEME_1);
-
-            }
-        };
-        item.setOnClickListener(buttonClickListener);
-    Button item=findViewById(R.id.button2);
-    View.OnClickListener button2ClickListener=new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            ThemeManager.changeTo(context,THEME_2);
-
-        }
-    };
-        item.setOnClickListener(button2ClickListener);
 }
+
+
+
